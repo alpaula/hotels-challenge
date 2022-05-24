9a18f2bbc291882de50e75224debfe0af876f8c3
Anna Luiza Araujo de Paula <annaluiza@MacBook-Pro-de-Anna.local>
2022-05-24 12:45:10 -0300

    fixing mock hotel data

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -7,6 +7,7 @@ import {hotelsData} from './hotels';
 export const handlers = [
   rest.get('/hotels', (req, res, ctx) => {
     return res(
+      ctx.delay(1500),
       ctx.status(200),
       ctx.json(hotelsData),
     )
