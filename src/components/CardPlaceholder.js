246d0d3ddaf408498f0807d37fcf88572c89339f
Anna Luiza Araujo de Paula <annaluiza@MacBook-Pro-de-Anna.local>
2022-05-24 21:44:44 -0300

    fixing styles

--- src/components/CardPlaceholder.js
+++ src/components/CardPlaceholder.js
@@ -1,12 +1,6 @@
 // Libs
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
-
-// Animations
-const placeholderAnimate = keyframes`
-	0% { background-position: 300% 0; }
-	100% { background-position: -300% 0; }
-`;
+import styled from 'styled-components';
 
 // Styles
 const Container = styled.li`
@@ -16,14 +10,11 @@ const Container = styled.li`
   height: 12rem;
   margin-bottom: 1rem;
   border-radius: 8px;
-  animation: ${placeholderAnimate} 3s infinite linear;
-  background-image: linear-gradient(to right, var(--app-white) 10%, #d8d8d8 70%, var(--app-white) 100%);
-  background-size: 150% 300%;
+  background-color: var(--app-white-transparent);
 `;
 
 const CardPlaceholder = () => (
-  <Container>
-  </Container>
+  <Container />
 );
 
 export default CardPlaceholder;
