fa94ea94a005a4365dad562183c0d0ed8ed1bc90
alpaula <anna.luiza.araujo@hotmail.com>
2022-06-01 13:33:13 -0300

    removing unused code

--- src/pages/Reserves.js
+++ src/pages/Reserves.js
@@ -1,5 +1,5 @@
 // Libs
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
